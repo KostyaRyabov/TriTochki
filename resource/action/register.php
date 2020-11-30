92c62dcab6fabb47ea2dd5d2e1f2e1a76a7ae136
Dmitry Naidich <dmitry@naidich.ru>
2020-11-30 22:08:07 +0300

    Обработка регистрации
    Теперь можно работать с запросами, используя плейсхолдеры для предупреждения возможных ошибок, в т. ч. sql-инъекций.
    По-другому обработаны приходящие POST данные.

--- resource/action/register.php
+++ resource/action/register.php
@@ -3,49 +3,52 @@
 	include($_SERVER["DOCUMENT_ROOT"]."/db.php");
 	include($_SERVER["DOCUMENT_ROOT"]."/functions.php");
 	
+	// Инициализация и обработка полей
+	$username = treat(trim($_POST["username"]));
+	$password = treat(trim($_POST["password"]));
+	$repeat = treat(trim($_POST["repeat_password"]));
+	$email = treat(trim($_POST["email"]));
+	
 	$errors = array(); // Массив для сбора ошибок
 	
 	// Первичные проверки полей на заполнение данных
-	if(!strlen(trim($_POST["username"]))) $errors["username"] = "Fill username field!";
-	if(!strlen(trim($_POST["password"]))) $errors["password"] = "Fill password field!";
-	if(!strlen(trim($_POST["repeat_password"]))) $errors["repeat_password"] = "Repeat your password!";
-	if(!strlen(trim($_POST["email"]))) $errors["email"] = "Fill email field!";
+	if(!strlen($username)) $errors["username"] = "Введите логин!";
+	if(!strlen($password)) $errors["password"] = "Введите пароль!";
+	if(!strlen($repeat)) $errors["repeat_password"] = "Введите пароль еще раз!";
+	if(!strlen($email)) $errors["email"] = "Введите email!";
 	
 	// Сразу прекращаем выполнение, если что-то из обязательного отсутствует
 	if(count($errors)) exit(json_encode($errors));
 	
-	// Устранение возможных уязвимостей
-	$_POST = defender($_POST);
-	
 	// Приведение логина и email к строчным буквам, чтобы не было повторов с разным типом букв
-	$_POST["username"] = strtolower($_POST["username"]);
-	$_POST["email"] = strtolower($_POST["email"]);
+	$username = strtolower($username);
+	$email = strtolower($email);
 	
 	// Не даем ввести меньше 8 символов
-	if(strlen($_POST["password"]) < 8) $errors["password"] = "Too short password's length!";
+	if(strlen($password) < 8) $errors["password"] = "Длина пароля должна быть не менее 8-ми символов!";
 	if(count($errors)) exit(json_encode($errors));
 	
 	// Проверка совпадения двух паролей
-	if($_POST["password"] != $_POST["repeat_password"]){
-		$errors["password"] = "Passwords are not same!";
-		$errors["repeat_password"] = "Passwords are not same!";
+	if($password != $repeat){
+		$errors["password"] = "Пароли не совпадают!";
+		$errors["repeat_password"] = "Пароли не совпадают!";
 		exit(json_encode($errors));
 	}
 	
 	// Проверяем, не занят ли логин или email
-	$res = query("SELECT Login, Email FROM user WHERE Login='".$_POST["username"]."' OR Email='".$_POST["email"]."'");
+	$res = DB::query("SELECT Login, Email FROM user WHERE Login=%s OR Email=%s", [$username, $email]);
 	$row = mysqli_fetch_array($res);
-	if($row["Login"] == $_POST["username"]) $errors["username"] = "This username already exists!";
-	if($row["Email"] == $_POST["email"]) $errors["email"] = "This email already exists!";
+	if($row["Login"] == $username) $errors["username"] = "Это имя пользователя уже занято!";
+	if($row["Email"] == $email) $errors["email"] = "Этот email уже занят!";
 	
 	// Выводим ошибки по последним проверкам
 	if(count($errors)) exit(json_encode($errors));
 	
 	DB::insert("user", [
-	 "Login" => $_POST["username"],
-	 "Password" => md5($_POST["password"]), //todo нужен другой метод защиты пароля
-	 "Email" => $_POST["email"],
-	 "Time" => time()
+	 "%s:Login" => $username,
+	 "%s:Password" => md5($password), //todo нужен другой метод защиты пароля
+	 "%s:Email" => $email,
+	 "%d:Time" => time()
 	]);
 	
 	// todo далее вход пользователя (после решения вопроса безопасности с cookie "id")
