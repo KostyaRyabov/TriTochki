d23757404558258fdc517d5d5d92d0e952b5f124
Dmitry Naidich <dmitry@naidich.ru>
2020-11-30 18:30:23 +0300

    Изменена функция для обработки значений
    + комментарии

--- functions.php
+++ functions.php
@@ -1,16 +1,12 @@
 <?php
-	// Обработка переданных значений на xss-уязвимости и sql-инъекции
-	function defender($method = array()){
-		foreach($method as $key => $value){
-			$value = preg_replace("/script/i", "", $value);
-			$value = preg_replace("/\w*((\%27)|(\'))((\%6F)|o|(\%4F))((\%72)|r|(\%52))/ix", "", $value);
-			$value = strip_tags($value);
-			$value = htmlentities($value, ENT_QUOTES);
-			
-			$method[$key] = $value;
-		}
+	// Обработка переданного значения для предупреждения уязвимостей
+	function treat($var){
+		$var = preg_replace("/script/i", "", $var); // Любое упоминание о кастомном js-скрипте агрессивно удаляется
+		$var = preg_replace("/\w*((\%27)|(\'))((\%6F)|o|(\%4F))((\%72)|r|(\%52))/ix", "", $var);
+		$var = strip_tags($var); // Обработка HTML и PHP тэгов
+		$var = htmlentities($var, ENT_QUOTES); // Преобразование спец.символов в HTML сущности
 		
-		return $method;
+		return $var;
 	}
 	
 	// Перевод даты и времени из mysql формата в человекопонятную
