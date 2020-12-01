4133ec78d56ea7de423311eebb16300df3f732ab
Dmitry Naidich <dmitry@naidich.ru>
2020-12-01 15:26:30 +0300

    Обработка и русификация логина

--- resource/action/login.php
+++ resource/action/login.php
@@ -3,31 +3,31 @@
 	include($_SERVER["DOCUMENT_ROOT"]."/db.php");
 	include($_SERVER["DOCUMENT_ROOT"]."/functions.php");
 	
+	// Инициализация и обработка полей
+	$username = treat(trim($_POST["username"]));
+	$password = treat(trim($_POST["password"]));
 	$errors = array(); // Массив для сбора ошибок
 	
 	// Первичные проверки полей на заполнение данных
-	if(!strlen(trim($_POST["username"]))) $errors["username"] = "Fill username field!";
-	if(!strlen(trim($_POST["password"]))) $errors["password"] = "Fill password field!";
-	
-	// Устранение возможных уязвимостей
-	$_POST = defender($_POST);
+	if(!strlen($username)) $errors["username"] = "Введите логин!";
+	if(!strlen($password)) $errors["password"] = "Введите пароль!";
 	
 	// Приведение логина к строчным буквам, чтобы не было повторов с разным типом букв
-	$_POST["username"] = strtolower($_POST["username"]);
+	$username = strtolower($username);
 	
 	// Не даем ввести меньше 8 символов
-	if(strlen($_POST["password"]) > 0 and strlen($_POST["password"]) < 8)
-		$errors["password"] = "Too short password's length!";
+	if(strlen($password) > 0 and strlen($password) < 8)
+		$errors["password"] = "Длина пароля должна быть не менее 8-ми символов!";
 	
 	// Если уже есть ошибки, прерываем выполнение, чтобы не дергать базу лишний раз
 	if(count($errors)) exit(json_encode($errors));
 	
 	// Проверка на корректность логина и пароля
-	$res = query("SELECT id_user, Password FROM user WHERE login='".$_POST["username"]."'");
+	$res = DB::query("SELECT id_user, Password FROM user WHERE login=%s", [$username]);
 	$row = mysqli_fetch_array($res);
 	if($row["Password"] != md5($_POST["password"])){ //todo такой же другой метод защиты пароля, как при регистрации
-		$errors["username"] = "Maybe wrong username?";
-		$errors["password"] = "Maybe wrong password?";
+		$errors["username"] = "Некорректные данные для входа!";
+		$errors["password"] = "Некорректные данные для входа!";
 		exit(json_encode($errors));
 	}
 	
