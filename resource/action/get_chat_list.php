fed97bc812634c0683a36182aaa294cc7a1868e2
Dmitry Naidich <dmitry@naidich.ru>
2020-12-07 16:35:39 +0300

    Группировка, когда не указан пользователь
    + комментарии

--- resource/action/get_chat_list.php
+++ resource/action/get_chat_list.php
@@ -11,17 +11,16 @@
 		// Получение текущего пользователя
 		$user_data = userData();
 		$id = $user_data["id"];
-		$where = "WHERE chat_users.id_user=%d";
-	} else $where = "";
+		$sql = "WHERE chat_users.id_user=%d";
+	} else $sql = "GROUP BY Name";
 	
-	$chats = array(); // Чаты пользователя
+	$chats = array(); // Список чатов
 	
-	// Выборка чатов, в которых состоит пользователь
+	// Выборка чатов
 	$sel = DB::query("
 		SELECT chat.id_chat, Name
 		FROM chat_users
-		INNER JOIN chat ON chat_users.id_chat=chat.id_chat ".$where,
-	 [$id]
+		INNER JOIN chat ON chat_users.id_chat=chat.id_chat ".$sql, [$id]
 	);
 	while($row = mysqli_fetch_array($sel)) $chats[$row["id_chat"]] = $row["Name"];
 	
