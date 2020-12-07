cbdab9dc827e3e86c9f1984019750739048d14e4
Dmitry Naidich <dmitry@naidich.ru>
2020-12-07 16:26:52 +0300

    Проверка на доступ к написанию сообщений

--- resource/action/send_message.php
+++ resource/action/send_message.php
@@ -15,6 +15,11 @@
 	$chat = treat(intval($_POST["chat"]));
 	if(!$chat) exit(json_encode(["error" => "Некорректный чат!"]));
 	
+	// Проверка пользователя на доступ к чату
+	$checksel = DB::query("SELECT id_chat FROM chat_users WHERE id_chat=%d AND id_user=%d", [$chat, $user]);
+	$check = mysqli_fetch_array($checksel);
+	if(!$check["id_chat"]) exit(json_encode(["error" => "Вы не авторизованы для этого чата!"]));
+	
 	// Получение текста
 	$text = treat(strval($_POST["text"]));
 	
