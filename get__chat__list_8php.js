33b8dff964a6d5560c4eec83055a67a5b4b9332f
Kinapak <Kinapak@users.noreply.github.com>
2020-12-07 13:38:16 +0000

    deploy: fed97bc812634c0683a36182aaa294cc7a1868e2

--- get__chat__list_8php.js
+++ get__chat__list_8php.js
@@ -2,5 +2,5 @@ var get__chat__list_8php =
 [
     [ "$chats", "get__chat__list_8php.html#a081ce42c7f5e88051206391d63f57194", null ],
     [ "$sel", "get__chat__list_8php.html#a7d0f17f147bb327e312b71312f3cf040", null ],
-    [ "$where", "get__chat__list_8php.html#ac1c411fb888a01c13861a37e7bb0822f", null ]
+    [ "$sql", "get__chat__list_8php.html#a2a3a4efb286bdcd790d46a564ea2e446", null ]
 ];
