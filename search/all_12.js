8886a9ff18aa0dfe4862bd539e515c50d5378a47
Kinapak <Kinapak@users.noreply.github.com>
2020-12-16 19:56:24 +0000

    deploy: 48d414c530b269aea48f09033f91b63f1a92d5b2

--- search/all_12.js
+++ search/all_12.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['tritochki_2e_2e_2e_20_2d_20простой_20и_20удобный_20чат_20для_20Ваших_20переговоров_143',['TriTochki... - простой и удобный чат для Ваших переговоров',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['tritochki_2e_2e_2e_20vulnerable_20_2d_20чат_2c_20содержащий_20уязвимости_143',['TriTochki... Vulnerable - чат, содержащий уязвимости',['../md__r_e_a_d_m_e.html',1,'']]],
   ['time_5fsearch_144',['time_search',['../_main_page_8js.html#afa329a00a6ec6142a36ec1b4b30ec41d',1,'MainPage.js']]],
   ['treat_145',['treat',['../functions_8php.html#aea5cdc1fe7bd0946a22e2d6d6dd93014',1,'functions.php']]]
 ];
