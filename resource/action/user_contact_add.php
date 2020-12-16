f17d1473b64ed9946cd15b2c960df32fc5bfcf81
Dmitry Naidich <dmitry@naidich.ru>
2020-12-16 17:52:43 +0300

    Использование id вместо логина

--- resource/action/user_contact_add.php
+++ resource/action/user_contact_add.php
@@ -13,17 +13,12 @@
 	if(!$user_id) exit("Ошибка авторизации!");
 	
 	// Контакт к добавлению
-	$contact = treat(strval($_POST["contact"]));
+	$contact = treat(intval($_POST["contact"]));
 	if(!$contact) exit("Некорректные данные!");
 	
-	// Получение идентификатора контакта по логину
-	$idsel = DB::query("SELECT id_user FROM user WHERE Login=%s", [$contact]);
-	$id = mysqli_fetch_array($idsel);
-	if(!$id["id_user"]) exit("Некорректные данные!");
-	
 	// Проверка на существование контакта в списке друзей у текущего пользователя
-	$checksel = DB::query("SELECT id FROM user_contacts WHERE id_user=%d AND id_contact=%d", [$user_id, $id["id_user"]]);
+	$checksel = DB::query("SELECT id FROM user_contacts WHERE id_user=%d AND id_contact=%d", [$user_id, $contact]);
 	$check = mysqli_fetch_array($checksel);
 	if($check["id"]) exit("Контакт уже есть в списке!");
 	
-	DB::insert("user_contacts", ["%d:id_user" => $user_id, "%d:id_contact" => $id["id_user"]]);
+	DB::insert("user_contacts", ["%d:id_user" => $user_id, "%d:id_contact" => $contact]);
