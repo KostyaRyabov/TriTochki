fe0f699571e87f876815891c21ff691dbda4c982
Dmitry Naidich <dmitry@naidich.ru>
2020-12-21 19:56:49 +0300

    Генерация рандомной соли
    + текстовые улучшения

--- resource/action/restore.php
+++ resource/action/restore.php
@@ -14,14 +14,15 @@
 	$row = mysqli_fetch_array($sel);
 	if(!$row["id_user"] or !$row["Email"]) exit(json_encode(["username" => "Данного пользователя не существует!"]));
 	
-	// Генерация кода восстановления
-	$code = str_shuffle(md5($row["id_user"].$row["Email"]));
-	if(!$code) exit(json_encode(["username" => "Данного пользователя не существует!"]));
+	// Генерация кода восстановления с солью
+	$salt = random_bytes(rand(8, 16));
+	$code = str_shuffle(md5($salt.$row["id_user"].$row["Email"]));
+	if(!strlen($code)) exit(json_encode(["username" => "Ошибка запроса..."]));
 	
 	DB::update("user", ["%s:restore_code" => $code], ["Login=%s", [$username]]);
 	
 	// Отправка сообщения на почту
 	$headers = "Content-type: text/html; charset=utf-8 \r\n";
 	$headers .= "From: TriTochki... <no-reply@tritochki.ru>\r\n";
-	$message = "Здравствуйте! Вы запросили восстановление доступа к чату ТриТочки... Чтобы сменить пароль, проследуйте по ссылке: http://dev.russiabase.ru/Restore.html?code=".$code;
+	$message = "Здравствуйте! Вы запросили восстановление доступа к чату \"ТриТочки...\" Чтобы сменить пароль, проследуйте по ссылке: http://dev.russiabase.ru/Restore.html?code=".$code;
 	mail($row["Email"], "Восстановление доступа", $message, $headers);
